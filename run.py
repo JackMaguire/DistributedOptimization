03db1dbb8c5f189e0588f5bffd730fcd19d72d83
Jack Maguire <jackmaguire1444@gmail.com>
2021-10-16 00:11:07 +0000

    Revert "glycine draft works"
    This reverts commit ec8ff0312932a154e1a0be98c0da3a3312babe85.

--- run.py
+++ run.py
@@ -6,12 +6,11 @@ from run_worker import *
 import argparse
 
 parser = argparse.ArgumentParser(description='Running Nevergrad on multiple CPUs using MPI')
-parser.add_argument('--pose', help='Pose to build off of', required=True, type=str )
-parser.add_argument('--NHres', help='PDB code for the O residue', required=True, type=str )
 parser.add_argument('--opt', help='optimizer to use', required=True )
 parser.add_argument('--budget', help='budget for optimizer', required=True, type=int )
 parser.add_argument('--hours', help='How long to run the simulation (budget just needs to be an estimate). This time does not include spin down time.', required=False, type=float, default=-1.0 )
 parser.add_argument('--out_prefix', help='prefix for output files', required=True, type=str )
+parser.add_argument('--in_prefices', help='comma separated list of prefices to load from', required=False, type=str, default="" )
 args = parser.parse_args()
 
 comm = MPI.COMM_WORLD
@@ -21,6 +20,6 @@ rank = comm.Get_rank()
 print( args.opt, rank )
 
 if rank == 0:
-    run_master( comm=comm, nprocs=nprocs, rank=rank, opt=args.opt, budget=args.budget, out_prefix=args.out_prefix, hours=args.hours )
+    run_master( comm=comm, nprocs=nprocs, rank=rank, opt=args.opt, budget=args.budget, out_prefix=args.out_prefix, in_prefices=args.in_prefices, hours=args.hours )
 else:
-    run_worker( comm, rank, out_prefix=args.out_prefix, posename=args.pose, NHres=args.NHres )
+    run_worker( comm, rank, out_prefix=args.out_prefix )
